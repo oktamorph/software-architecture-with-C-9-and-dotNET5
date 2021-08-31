f178670361db80f3436bfb59e531686af111b4b2
Oleksandr Bolilyi <oleksandr.bolilyi@hitachi-powergrids.com>
2021-08-31 15:30:05 +0200

    update the structure of the project

---
+++ chapter8/WWTravelClubDB/MainDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace WWTravelClubDB
+{
+    public class MainDbContext : DbContext
+    {
+        public MainDbContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder) { }
+    }
+
+}
