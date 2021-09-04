2c3d2db77b429f463b82ebab15b80ddbdab5a9d8
Oleksandr Bolilyi <oleksandr.bolilyi@hitachi-powergrids.com>
2021-09-04 12:48:59 +0200

    update the "Destination", "Package", and "PackagesListDTO" classes

--- chapter8/WWTravelClubDBCosmo/Models/Destination.cs
+++ chapter8/WWTravelClubDBCosmo/Models/Destination.cs
@@ -1,15 +1,13 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WWTravelClubDB.Models
 {
     public class Destination
     {
-        public int Id { get; set; }
-        [MaxLength(128), Required]
+        [Key]
+        public string Id { get; set; }
         public string Name { get; set; }
-        [MaxLength(128), Required]
         public string Country { get; set; }
         public string Description { get; set; }
         public ICollection<Package> Packages { get; set; }
--- chapter8/WWTravelClubDBCosmo/Models/Package.cs
+++ chapter8/WWTravelClubDBCosmo/Models/Package.cs
@@ -1,23 +1,20 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using WWTravelClubDB.Models;
 
 namespace WWTravelClubDB.Models
 {
     public class Package
     {
-        public int Id { get; set; }
-        [MaxLength(128), Required]
+        [Key]
+        public string Id { get; set; }
         public string Name { get; set; }
-        [MaxLength(128)]
         public string Description { get; set; }
         public decimal Price { get; set; }
         public int DurationInDays { get; set; }
         public DateTime? StartValidityDate { get; set; }
         public DateTime? EndValidityDate { get; set; }
         public Destination MyDestination { get; set; }
-        public int DestinationId { get; set; }
+        public string DestinationId { get; set; }
     }
 
 }
