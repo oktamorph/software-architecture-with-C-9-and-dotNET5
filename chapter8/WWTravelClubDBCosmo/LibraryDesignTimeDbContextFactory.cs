7fb42ae194c182258115a0f3449dbea7d99f4095
Oleksandr Bolilyi <oleksandr.bolilyi@hitachi-powergrids.com>
2021-09-04 18:37:35 +0200

    update the "LibraryDesignTimeDbContextFactory" class

--- chapter8/WWTravelClubDBCosmo/LibraryDesignTimeDbContextFactory.cs
+++ chapter8/WWTravelClubDBCosmo/LibraryDesignTimeDbContextFactory.cs
@@ -5,13 +5,14 @@ namespace WWTravelClubDB
 {
     public class LibraryDesignTimeDbContextFactory : IDesignTimeDbContextFactory<MainDbContext>
     {
-        private const string connectionString = @"Server=SESTO-L2103005;Database=wwtravelclub;
-                                                    Trusted_Connection=True;MultipleActiveResultSets=true";
+        private const string endpoint = "https://oktamorph.documents.azure.com:443/";
+        private const string key = "iUOGiBNEAX2dH0y5gKP1Nv3BDrrrOVtPzFQ4RiCbjRMPnySh6U64VauHQH9M6tTl3LgxOMBWVBthX6qEt65wqA==";
+        private const string databaseName = "packagesdb";
 
         public MainDbContext CreateDbContext(params string[] args)
         {
             var builder = new DbContextOptionsBuilder<MainDbContext>();
-            builder.UseSqlServer(connectionString);
+            builder.UseCosmos(endpoint, key, databaseName);
 
             return new MainDbContext(builder.Options);
         }
