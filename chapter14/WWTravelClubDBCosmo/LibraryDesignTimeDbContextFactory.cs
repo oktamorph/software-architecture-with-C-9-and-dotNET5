0af375baa65d26bd21294b3197b5e489467c7b21
Oleksandr Bolilyi <oleksandr.bolilyi@hitachi-powergrids.com>
2021-09-20 15:12:11 +0200

    rebuild the project

---
+++ chapter14/WWTravelClubDBCosmo/LibraryDesignTimeDbContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace WWTravelClubDB
+{
+    public class LibraryDesignTimeDbContextFactory : IDesignTimeDbContextFactory<MainDbContext>
+    {
+        private const string endpoint = "https://oktamorph.documents.azure.com:443/";
+        private const string key = "iUOGiBNEAX2dH0y5gKP1Nv3BDrrrOVtPzFQ4RiCbjRMPnySh6U64VauHQH9M6tTl3LgxOMBWVBthX6qEt65wqA==";
+        private const string databaseName = "packagesdb";
+
+        public MainDbContext CreateDbContext(params string[] args)
+        {
+            var builder = new DbContextOptionsBuilder<MainDbContext>();
+            builder.UseCosmos(endpoint, key, databaseName);
+
+            return new MainDbContext(builder.Options);
+        }
+    }
+
+}
---
+++ chapter14/WWTravelClubDBCosmo/MainDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using WWTravelClubDB.Models;
+
+namespace WWTravelClubDB
+{
+    public class MainDbContext : DbContext
+    {
+        public DbSet<Package> Packages { get; set; }
+        public DbSet<Destination> Destinations { get; set; }
+
+        public MainDbContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Destination>()
+                .OwnsMany(m => m.Packages);
+
+            builder.Entity<Destination>()
+                .HasIndex(m => m.Country);
+
+            builder.Entity<Destination>()
+                .HasIndex(m => m.Name);
+        }
+
+    }
+
+}
