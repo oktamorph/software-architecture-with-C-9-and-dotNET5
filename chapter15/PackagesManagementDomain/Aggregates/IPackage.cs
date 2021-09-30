87e5ae4e82a44c4f1a7a88d7c78505b9c43373eb
Oleksandr Bolilyi <oleksandr.bolilyi@hitachi-powergrids.com>
2021-09-30 21:11:14 +0200

    add the "Aggregates",  "DTOs", and "IRepository" folders

--- chapter15/PackagesManagementDomain/Aggregates/IPackage.cs
+++ chapter15/PackagesManagementDomain/Aggregates/IPackage.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using PackagesManagementDomain.DTOs;
 
 namespace PackagesManagementDomain.Aggregates
 {
---
+++ chapter15/PackagesManagementDomain/IRepository/IPackageEventRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PackagesManagementDomain.IRepository
+{
+    interface IPackageEventRepository
+    {
+    }
+}
