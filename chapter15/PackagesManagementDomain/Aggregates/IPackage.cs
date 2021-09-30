2e6466e149f0544264d22c0e8ac300736b509c1f
Oleksandr Bolilyi <oleksandr.bolilyi@hitachi-powergrids.com>
2021-09-30 21:01:15 +0200

    add the "Aggregates" folder

---
+++ chapter15/PackagesManagementDomain/Aggregates/IPackage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PackagesManagementDomain.Aggregates
+{
+    public interface IPackage : IEntity<int>
+    {
+        void FullUpdate(IPackageFullEditDTO o);
+        string Name { get; set; }
+
+        string Description { get; set; }
+        decimal Price { get; set; }
+        int DurationInDays { get; }
+        DateTime? StartValidityDate { get;  }
+        DateTime? EndValidityDate { get; }
+        int DestinationId { get; }
+    }
+
+}
