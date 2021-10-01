93dd701475cb147f017077dc5b11554481d5fa88
Oleksandr Bolilyi <oleksandr.bolilyi@hitachi-powergrids.com>
2021-10-01 10:21:03 +0200

    add the "Queries" folder

---
+++ chapter15/PackagesManagement/Models/PackageFullEditViewModel.cs
@@ -0,0 +1,47 @@
+﻿using PackagesManagementDB.Models;
+using PackagesManagementDomain.Aggregates;
+using PackagesManagementDomain.DTOs;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PackagesManagement.Models
+{
+    public class PackageFullEditViewModel : IPackageFullEditDTO
+    {
+        public PackageFullEditViewModel() { }
+        public PackageFullEditViewModel(IPackage o)
+        {
+            Id = o.Id;
+            DestinationId = o.DestinationId;
+            Name = o.Name;
+            Description = o.Description;
+            Price = o.Price;
+            DurationInDays = o.DurationInDays;
+            StartValidityDate = o.StartValidityDate;
+            EndValidityDate = o.EndValidityDate;
+        }
+        public int Id { get; set; }
+        [Display(Name = "name")]
+        [StringLength(128, MinimumLength = 5), Required]        
+        public string Name { get; set; }
+        [Display(Name = "package infos")]
+        [StringLength(128, MinimumLength = 10), Required]        
+        public string Description { get; set; }
+        [Display(Name="price")]
+        [Range(0, 100000)]
+        public decimal Price { get; set; }
+        [Display(Name = "duration in days")]
+        [Range(1, 90)]
+        public int DurationInDays { get; set; }
+        [Display(Name = "available from")]
+        public DateTime? StartValidityDate { get; set; }
+        [Display(Name = "available to")]
+        public DateTime? EndValidityDate { get; set; }
+        [Display(Name = "destination")]
+        public int DestinationId { get; set; }
+    }
+
+}
---
+++ chapter15/PackagesManagement/Models/PackageInfosViewModel.cs
@@ -0,0 +1,27 @@
+﻿using PackagesManagementDB.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PackagesManagement.Models
+{
+    public class PackageInfosViewModel
+    {
+        public int Id { get; set; }
+        public string DestinationName { get; set; }
+        [MaxLength(128), Required]
+        public string Name { get; set; }
+        [MaxLength(128)]
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int DurationInDays { get; set; }
+        public DateTime? StartValidityDate { get; set; }
+        public DateTime? EndValidityDate { get; set; }
+        public Destination MyDestination { get; set; }
+        [ConcurrencyCheck]
+        public long EntityVersion { get; set; }
+        public int DestinationId { get; set; }
+    }
+}
