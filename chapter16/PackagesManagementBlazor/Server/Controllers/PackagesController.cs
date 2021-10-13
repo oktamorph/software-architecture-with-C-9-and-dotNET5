c1afbea99b78f3c89c55673fd283f7868cbcf3f9
Oleksandr Bolilyi <oleksandr.bolilyi@hitachi-powergrids.com>
2021-10-13 22:21:17 +0200

    add the "Queries" folder to the "PackagesManagementBlazor.Server" project

---
+++ chapter16/PackagesManagementBlazor/Server/Controllers/PackagesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PackagesManagementBlazor.Server.Queries;
+using PackagesManagementBlazor.Shared;
+
+namespace PackagesManagementBlazor.Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PackagesController : ControllerBase
+    {
+        // GET api/<PackagesController>/flor
+        [HttpGet("{location}")]
+        public async Task<PackagesListViewModel> Get(string location, [FromServices] IPackagesListByLocationQuery query)
+        {
+            return new PackagesListViewModel
+            {
+                Items = await query.GetPackagesOf(location)
+            };
+        }
+    }
+}
---
+++ chapter16/PackagesManagementBlazor/Server/Queries/IPackagesListByLocationQuery.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DDD.ApplicationLayer;
+using PackagesManagementBlazor.Shared;
+
+namespace PackagesManagementBlazor.Server.Queries
+{
+    public interface IPackagesListByLocationQuery : IQuery
+    {
+        Task<IEnumerable<PackageInfosViewModel>> GetPackagesOf(string location);
+    }
+
+}
