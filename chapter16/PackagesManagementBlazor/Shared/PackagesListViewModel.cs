787b9b36e525f848a9bd50285e9c54c9b4551b77
Oleksandr Bolilyi <oleksandr.bolilyi@hitachi-powergrids.com>
2021-10-13 22:08:25 +0200

    add the models in the "PackagesManagementBlazor.Shared" project

---
+++ chapter16/PackagesManagementBlazor/Shared/PackagesListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackagesManagementBlazor.Shared
+{
+    public class PackagesListViewModel
+    {
+        public IEnumerable<PackageInfosViewModel> Items { get; set; }
+    }
+
+}
