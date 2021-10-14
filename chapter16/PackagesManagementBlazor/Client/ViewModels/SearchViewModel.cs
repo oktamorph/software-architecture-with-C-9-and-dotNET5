f10b5349b4e2598cec4098722a40227af725ea09
Oleksandr Bolilyi <oleksandr.bolilyi@hitachi-powergrids.com>
2021-10-14 21:57:27 +0200

    Add the "Services" and "ViewModels" folders with its content

---
+++ chapter16/PackagesManagementBlazor/Client/ViewModels/SearchViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PackagesManagementBlazor.Client.ViewModels
+{
+    public class SearchViewModel
+    {
+        [Required]
+        public string Location { get; set; }
+    }
+}
